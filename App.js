b0f8bbbc187cf4de8757110811a167b001b4887c
Web&Mobile Star <topdeveloper1212@gmail.com>
2017-10-11 21:52:37 +0800

    implement mask image to viewfinder

--- App.js
+++ App.js
@@ -1,104 +1,124 @@
 import React from 'react';
-import { ImageEditor, StyleSheet, Text, View, Image, Button, TouchableOpacity, Dimensions, Alert } from 'react-native';
-import { ImagePicker } from 'expo';
-import vision from 'react-cloud-vision-api';
-import mergeImages from 'merge-images';
+import { StyleSheet, Text, View, TouchableOpacity, Image, Dimensions, Alert } from 'react-native';
+import { Camera, Permissions, FileSystem } from 'expo';
 
 export default class App extends React.Component {
   state = {
-    image: null,
-    isMask: false
-  };
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back
+  }
 
-  componentWillMount() {
-    vision.init({auth: 'AIzaSyCLt-6tFD-Pk8LTQMjG8al_itbAT2M6m_Q'})
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({
+      hasCameraPermission: status === 'granted'
+    })
+  }
+
+  _takeImage(){
+    if (this.camera) {
+      this.camera.takePictureAsync().then(data => {
+        const image = {
+          uri: data.uri,
+          type: 'image/jpeg',
+          name: 'myImage' + '-' + Date.now() + '.jpg'
+        }
+
+        const imgBody = new FormData()
+        imgBody.append('image', image);
+
+        // Here We need to put backend API for OCR and that API need to return OCR's result
+        // On client side, It will send the image data to server
+
+        
+        const url = '';
+        fetch(url, {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'multipart/form-data',
+          },
+          body: imgBody
+        }).then(res=>res.json()).then(result => {
+          Alert("Result", result)
+        }).catch(error=>{
+          Alert("Error", "Error occured")
+        })
+      });
+    }
   }
 
   render() {
-    let { image, isMask } = this.state;
-
-    return (
-      <View style={styles.container}>
-      {image &&
-        <Image source={{ uri: image }} style={styles.backgroundImage}>
-          {
-            isMask?(<Image source={require('./resource/image/Electric_meter_mask.png')} style={styles.mask}/>):null
-          }
-        </Image>
-      }
-        <Button
-          title="Select Image"
-          onPress={this._pickImage}
-        />
-        <Button
-          title="Mask Image"
-          onPress={this._maskImage}
-        />
-        <Button
-          title="Send Image"
-          onPress={this._sendImage}
-        />
-
-      </View>
-    );
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View/>
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to Camera</Text>
+    } else {
+      return (
+        <View style={styles.container}>
+          <Camera 
+            ref={ref => {
+              this.camera = ref;
+            }} style={styles.camera} type={this.state.type}>
+            <Image source={require('./image/mask.png')} style={styles.mask}/>
+            <View style={styles.bottom}>
+              <TouchableOpacity onPress={()=>this._takeImage()}>
+                <Image source={require('./image/take.png')}  style={styles.take}/>
+              </TouchableOpacity>
+              <View style={styles.rebase}>
+                <TouchableOpacity onPress={()=>{
+                  this.setState({
+                    type: this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                  });
+                }}>
+                  <Image source={require('./image/cycle.png')} style={styles.rebaseImage}/>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
   }
-
-  _pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      allowsEditing: true,
-      aspect: [4, 3],
-    });
-
-    console.log(result);
-
-    if (!result.cancelled) {
-      this.setState({ image: result.uri });
-    }
-  };
-
-  _maskImage = async () => {
-    this.setState({
-      isMask: true
-    })
-  };
-
-  _sendImage = async () => {
-    if (this.state.image == null) {
-      Alert.alert("Notice", "Please select image first")
-      return
-    }
-
-    const req = new vision.Request({
-      image: new vision.Image({
-        path:  this.state.image
-          // url: 'https://scontent-nrt1-1.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/12353236_1220803437936662_68557852_n.jpg'
-      }),
-      features: [
-        new vision.Feature('TEXT_DETECTION', 100)
-      ]
-    })
-    vision.annotate(req).then((res)=>{
-      Alert.alert("Resut", JSON.stringify(res.responses))
-    }, (e)=>{
-      Alert.alert("Error", e)
-    })   
-  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  camera: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'column'
   },
-  backgroundImage: {
-    width: Dimensions.get('window').width - 100,
-    height: 300,
+  mask: {
+    width: Dimensions.get('window').width-50,
+    height: Dimensions.get('window').width-50
+  },
+  bottom: {
+    width: Dimensions.get('window').width,
+    position: 'absolute',
+    flexDirection: 'row',
+    bottom: 10,
     alignItems: 'center',
     justifyContent: 'center'
   },
-  mask: {
-    position:'absolute'
+  take: {
+    width:80,
+    height: 80,
+  },
+  rebase: {
+    position: 'absolute',
+    right: 10
+    // marginRight: 10,
+  },
+  rebaseImage: {
+    width: 40,
+    resizeMode: 'contain'
   }
 });
