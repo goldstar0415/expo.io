c1e8e91cd70040cb49302269a5935e86f43e93b4
Web&Mobile Star <topdeveloper1212@gmail.com>
2017-10-14 10:59:37 +0800

    complete first phase

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image, Dimensions, Alert } from 'react-native';
-import { Camera, Permissions, FileSystem } from 'expo';
+import { Camera, Permissions } from 'expo';
+import axios from 'axios';
+import Spinner from 'react-native-loading-spinner-overlay';
+
+const cloudVisionKey = 'AIzaSyCLt-6tFD-Pk8LTQMjG8al_itbAT2M6m_Q';
+const cloudVision  = 'https://vision.googleapis.com/v1/images:annotate?key=' + cloudVisionKey;
+
 
 export default class App extends React.Component {
-  state = {
-    hasCameraPermission: null,
-    type: Camera.Constants.Type.back
+  constructor(props) {
+    super(props);
+    this.state = {
+      captureText: null,
+      showLoader: false,
+      hasCameraPermission: null,
+      type: Camera.Constants.Type.back
+    };
+    this.toggleLoader = this.toggleLoader.bind(this);
+    this.showAlert = this.showAlert.bind(this);
   }
 
   async componentWillMount() {
@@ -15,41 +28,58 @@ export default class App extends React.Component {
     })
   }
 
+  toggleLoader() {
+    this.setState({
+      showLoader: !this.state.showLoader
+    })
+  }
+
+  showAlert(title, message) {
+    this.toggleLoader();
+    setTimeout(()=>{
+      Alert.alert(
+        title,
+        message,
+        [
+          {text: 'Retake', onPress: () => console.log('OK Pressed')},
+        ]
+      )
+    }, 100);
+  }
+
   _takeImage(){
     if (this.camera) {
-      this.camera.takePictureAsync().then(data => {
-        const image = {
-          uri: data.uri,
-          type: 'image/jpeg',
-          name: 'myImage' + '-' + Date.now() + '.jpg'
-        }
-
-        const imgBody = new FormData()
-        imgBody.append('image', image);
-
-        // Here We need to put backend API for OCR and that API need to return OCR's result
-        // On client side, It will send the image data to server
-
-        
-        const url = '';
-        fetch(url, {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'multipart/form-data',
-          },
-          body: imgBody
-        }).then(res=>res.json()).then(result => {
-          Alert("Result", result)
-        }).catch(error=>{
-          Alert("Error", "Error occured")
-        })
-      });
+      let _this = this;
+      this.toggleLoader();
+      this.camera.takePictureAsync({base64:true}).then((data)=>{
+        axios.post(cloudVision, {
+          "requests":[{
+            "image": {
+              "content": data.base64
+            },
+            "features":[{
+              "type": "TEXT_DETECTION",
+              "maxResults": 1
+            }]
+          }]
+        }).then(function(response) {
+          if (response.data.responses[0].textAnnotations == undefined) {
+            _this.showAlert("Notice", "There is no character on image or you need to take photo more clearly");
+          } else {
+            let textAnnotations  = response.data.responses[0].textAnnotations[0];
+            let textContent = textAnnotations.description;
+            _this.showAlert("Success", textContent);
+          }
+        }).catch(function (error) {
+          _this.showAlert("Error", "Error occured!. It's related with OCR or Network connection problem. Please check and try it again");
+        });
+      })
+      .catch(err => console.error(err));
     }
   }
 
   render() {
-    const { hasCameraPermission } = this.state;
+    const { hasCameraPermission, showLoader } = this.state;
     if (hasCameraPermission === null) {
       return <View/>
     } else if (hasCameraPermission === false) {
@@ -57,13 +87,15 @@ export default class App extends React.Component {
     } else {
       return (
         <View style={styles.container}>
+         
           <Camera 
             ref={ref => {
               this.camera = ref;
             }} style={styles.camera} type={this.state.type}>
+             <Spinner visible={showLoader}/>
             <Image source={require('./image/mask.png')} style={styles.mask}/>
             <View style={styles.bottom}>
-              <TouchableOpacity onPress={()=>this._takeImage()}>
+              <TouchableOpacity onPress={this._takeImage.bind(this)}>
                 <Image source={require('./image/take.png')}  style={styles.take}/>
               </TouchableOpacity>
               <View style={styles.rebase}>
